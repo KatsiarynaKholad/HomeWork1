5f0eb957d380c0a7740bd0b0167952f0496924ba
KatsiarynaKholad <100599536+KatsiarynaKholad@users.noreply.github.com>
2022-05-27 14:19:39 +0300

    Add project files.

---
+++ DaysOfWeek/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace DayOfWeek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program print the days of week to the Console");
+            Console.WriteLine("Please choose a number:");
+            Console.WriteLine("1: Monday");
+            Console.WriteLine("2: Tuesday");
+            Console.WriteLine("3: Wednesday");
+            Console.WriteLine("4: Thursday");
+            Console.WriteLine("5: Friday");
+            Console.WriteLine("6: Saturday");
+            Console.WriteLine("7: Sunday");
+
+            var number = 0;
+
+            var isNumber = int.TryParse(Console.ReadLine(), out number);
+
+            if (isNumber)
+            {
+                PrintDay(number);
+            }
+            else
+            {
+                Console.WriteLine("You can enter numbers 1-7");
+            }
+        }
+        static void PrintDay(int a)
+        {
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("It is Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("It is Thursday");
+                    break;
+                case 3:
+                    Console.WriteLine("It is Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("It is Tuesday");
+                    break;
+                case 5:
+                    Console.WriteLine("It is Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("It is Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("It is Sunday");
+                    break;
+
+                default:
+                    Console.WriteLine("I don't know what you do.");
+                    break;
+            }
+        }
+    }
+}
---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MultiplicationTable2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program print the multiplication table to the Console");
+            Console.WriteLine("Please enter a number:");
+
+            var number = 0;
+            var isNumber = int.TryParse(Console.ReadLine(), out number);
+            if (isNumber)
+            {
+                PrintMultiplicationTable(number);
+            }
+            else
+            {
+                Console.WriteLine("it isn't a number");
+            }
+        }
+        static void PrintMultiplicationTable(int a)
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                Console.WriteLine($"{a} * {i} = {a * i}");
+            }
+        }
+    }
+}
